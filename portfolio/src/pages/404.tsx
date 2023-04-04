31a42d012dcaeb76faff162bdebe641cf9e7e484
trevorichi <trevorichi@gmail.com>
2023-04-03 22:37:59 -0400

    added viewport to fix the resizing issue i was having, website is now psuedo mobile friendly

--- portfolio/src/pages/404.tsx
+++ portfolio/src/pages/404.tsx
@@ -3,7 +3,9 @@ export default function NotFound() {
   return (
     <>
       <h1>Testing 404</h1>
-      <Link href="/components/Home">GO HOME</Link>
+      <Link href="/components/Home" className="">
+        GO HOME
+      </Link>
     </>
   );
 }
